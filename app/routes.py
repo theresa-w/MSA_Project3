29e0da1737e7f9c247b63415281f631ac7986534
Theresa Wang <theresa.w0285@gmail.com>
2020-09-10 17:46:20 +1000

    add new entry

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
-from app import app, models
-from flask import render_template
+from app import app, models, db
+from flask import render_template, request, redirect, url_for
 
 Diary = models.Diary
 
@@ -8,3 +8,21 @@ Diary = models.Diary
 def index():
     entries = Diary.query.all()
     return render_template("index.html", entries = entries)
+
+
+
+# POST (Forms)
+@app.route('/diary/', methods=['POST'])
+def add_item():
+    # Get data from form fields taskName and taskDescription
+    diaryDate = request.form.get('diaryDate')
+    diaryTitle = request.form.get('diaryTitle')
+    diaryText = request.form.get('diaryText')
+    
+    # Put data into a new Task item
+    new_entry = Diary(date=diaryDate, title=diaryTitle, text=diaryText)
+    
+    # Add and commit the changes to the database
+    db.session.add(new_entry)
+    db.session.commit()
+    return redirect(url_for('index'))
